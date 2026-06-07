ac5f7c46ba26446cb1152d4e9e288bf99dccecb3
ArfanisP <panagiotisarfanis80@icloud.com>
2026-06-07 17:53:25 +0300

    Protected routes and JWT middleware added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -73,7 +73,12 @@ const loginUser = async (req, res) => {
   }
 };
 
+const getUserProfile = async (req, res) => {
+  res.json(req.user);
+};
+
 module.exports = {
   registerUser,
   loginUser,
+  getUserProfile,
 };
